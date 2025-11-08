a57bce860e6c450d3f8c9adba5f5da4efbac334d
Brian Maina <brayahnmainah@gmail.com>
2025-11-08 21:27:53 +0300

    sample app ready to go

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-rafce
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req,res) => {
+    res.json({message: 'Hello from a container',
+        service: 'hello-node',
+        pod: process.env.POD_NAME || 'unknown',
+        time: new Date().toISOString(),
+    }
+        
+    );
+})
+
+app.listen (PORT, ()=> {
+    console.log(`Demo app listening on port ${PORT}!`);
+})
